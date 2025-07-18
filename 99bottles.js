85bf4aa3516e90d8b00f59abd29e917cd1885088
jjm <npngjjm>
2025-07-18 20:41:01 +0900

    1.1.2 완성

--- 99bottles.js
+++ 99bottles.js
@@ -1,3 +1,27 @@
+const NoMore = verse =>
+  'No more bottles of beer on the wall, ' +
+  'no more bottles of beer.\n' +
+  'Go to the store and buy some more' +
+  '99 bottles of beer on the wall.\n';
+
+const LastOne = verse =>
+  '1 bottle of beer on the wall, ' +
+  '1 bottle of beer.\n' +
+  'Take it down and pss it around' +
+  'no more bottles of beer on the wall.\n';
+
+const Penultimate = verse =>
+  '2 bottles of beer on the wall, ' +
+  '2 bottles of beer.\n' +
+  'Take one down and pss it around' +
+  '1 bottle of beer on the wall.\n';
+
+const Default = verse =>
+  `${verse.number} bottles of beer on the wall, ` +
+  `${verse.number} bottles of beer.\n` +
+  'Take one down and pss it around' +
+  `${verse.number- 1} bottles of beer on the wall.\n`;
+  
 class Bottles {
   song() {
     return this.verses(99, 0);
