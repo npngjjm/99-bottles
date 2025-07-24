bd86634ee801e3494b46ae40d723d0ce4b486370
npngjjm <67635682+npngjjm@users.noreply.github.com>
2025-07-24 06:45:19 +0000

    flocking

---
+++ 99bottles.test.js
@@ -0,0 +1,162 @@
+import { describe, test, expect } from "vitest";
+
+function capitalize(str) {
+  if (!str) return "";
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+class Bottles {
+  song() {
+    return this.verses(99, 0);
+  }
+
+  verses(upper, lower) {
+    return downTo(upper, lower)
+      .map((i) => this.verse(i))
+      .join("\n");
+  }
+
+  verse(number) {
+    switch (number) {
+      case 0:
+        return (
+          `${capitalize(this.quantity(number))} ` +
+          `bottles of beer on the wall, ` +
+          "no more bottles of beer.\n" +
+          `${this.action(number)}` +
+          "99 bottles of beer on the wall.\n"
+        );
+      default:
+        return (
+          `${capitalize(this.quantity(number))} ` +
+          `${this.container(number)} ` +
+          `of beer on the wall, ` +
+          `${number} ${this.container(number)} of beer.\n` +
+          `${this.action(number)}` +
+          `${this.quantity(number - 1)} ` +
+          `${this.container(number - 1)} ` +
+          "of beer on the wall.\n"
+        );
+    }
+  }
+
+  container(number) {
+    if (number === 1) {
+      return "bottle";
+    } else {
+      return "bottles";
+    }
+  }
+
+  pronoun(number) {
+    if (number === 1) {
+      return "it";
+    } else {
+      return "one";
+    }
+  }
+
+  quantity(number) {
+    if (number === 0) {
+      return "no more";
+    } else {
+      return number.toString();
+    }
+  }
+
+  action(number) {
+    if (number === 0) {
+      return "Go to the store and buy some more, ";
+    } else {
+      return `Take ${this.pronoun(number)} down and pass it around, `;
+    }
+  }
+}
+
+const downTo = (max, min) => {
+  const numbers = [];
+  for (let n = max; n >= min; n--) {
+    numbers.push(n);
+  }
+  return numbers;
+};
+
+describe("Bottles", () => {
+  test("the first verse", () => {
+    const expected =
+      "99 bottles of beer on the wall, " +
+      "99 bottles of beer.\n" +
+      "Take one down and pass it around, " +
+      "98 bottles of beer on the wall.\n";
+    expect(new Bottles().verse(99)).toBe(expected);
+  });
+
+  test("another verse", () => {
+    const expected =
+      "3 bottles of beer on the wall, " +
+      "3 bottles of beer.\n" +
+      "Take one down and pass it around, " +
+      "2 bottles of beer on the wall.\n";
+    expect(new Bottles().verse(3)).toBe(expected);
+  });
+
+  test("verse 2", () => {
+    const expected =
+      "2 bottles of beer on the wall, " +
+      "2 bottles of beer.\n" +
+      "Take one down and pass it around, " +
+      "1 bottle of beer on the wall.\n";
+    expect(new Bottles().verse(2)).toBe(expected);
+  });
+
+  test("verse 1", () => {
+    const expected =
+      "1 bottle of beer on the wall, " +
+      "1 bottle of beer.\n" +
+      "Take it down and pass it around, " +
+      "no more bottles of beer on the wall.\n";
+    expect(new Bottles().verse(1)).toBe(expected);
+  });
+
+  test("verse 0", () => {
+    const expected =
+      "No more bottles of beer on the wall, " +
+      "no more bottles of beer.\n" +
+      "Go to the store and buy some more, " +
+      "99 bottles of beer on the wall.\n";
+    expect(new Bottles().verse(0)).toBe(expected);
+  });
+
+  test("a couple verses", () => {
+    const expected =
+      "99 bottles of beer on the wall, " +
+      "99 bottles of beer.\n" +
+      "Take one down and pass it around, " +
+      "98 bottles of beer on the wall.\n" +
+      "\n" +
+      "98 bottles of beer on the wall, " +
+      "98 bottles of beer.\n" +
+      "Take one down and pass it around, " +
+      "97 bottles of beer on the wall.\n";
+    expect(new Bottles().verses(99, 98)).toBe(expected);
+  });
+
+  test("a few verses", () => {
+    const expected =
+      "2 bottles of beer on the wall, " +
+      "2 bottles of beer.\n" +
+      "Take one down and pass it around, " +
+      "1 bottle of beer on the wall.\n" +
+      "\n" +
+      "1 bottle of beer on the wall, " +
+      "1 bottle of beer.\n" +
+      "Take it down and pass it around, " +
+      "no more bottles of beer on the wall.\n" +
+      "\n" +
+      "No more bottles of beer on the wall, " +
+      "no more bottles of beer.\n" +
+      "Go to the store and buy some more, " +
+      "99 bottles of beer on the wall.\n";
+    expect(new Bottles().verses(2, 0)).toBe(expected);
+  });
+});
