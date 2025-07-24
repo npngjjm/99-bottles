ee925eb0044f775238230696ba73ae0b121395f3
jjm <npngjjm>
2025-07-24 14:52:32 +0900

    flocking LSP

--- 99bottles.js
+++ 99bottles.js
@@ -13,14 +13,17 @@ class Bottles {
     switch (number) {
       case 0:
         return (
-          "No more bottles of beer on the wall, " +
+          `${capitalize(this.quantity(number))} ` +
+          `bottles of beer on the wall, ` +
           "no more bottles of beer.\n" +
           "Go to the store and buy some more" +
           "99 bottles of beer on the wall.\n"
         );
       default:
         return (
-          `${number} ${this.container(number)} of beer on the wall, ` +
+          `${capitalize(this.quantity(number))} ` +
+          `${this.container(number)} ` +
+          `of beer on the wall, ` +
           `${number} ${this.container(number)} of beer.\n` +
           `Take ${this.pronoun(number)} down and pass it around` +
           `${this.quantity(number-1)} ` +
@@ -52,7 +55,7 @@ class Bottles {
     if (number === 0) {
       return "no more";
     } else {
-      return number;
+      return number.toString();
     }
   }
 }
