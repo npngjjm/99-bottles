943e4b165695cc90bf3bf5d9193bfd65b3fe9176
npngjjm <67635682+npngjjm@users.noreply.github.com>
2025-07-31 07:18:11 +0000

    meta factory

--- 99bottles.test.js
+++ 99bottles.test.js
@@ -6,23 +6,6 @@ function capitalize(str) {
 }
 
 class Bottles {
-  bottleNumberFor(number) {
-    let bottleNumberClass;
-    switch (number) {
-      case 0:
-        bottleNumberClass = BottleNumber0;
-        break;
-      case 1:
-        bottleNumberClass = BottleNumber1;
-        break;
-      default:
-        bottleNumberClass = BottleNumber;
-        break;
-    }
-
-    return new bottleNumberClass(number);
-  }
-
   song() {
     return this.verses(99, 0);
   }
@@ -53,16 +36,10 @@ class BottleNumber {
 
   static for(number) {
     let bottleNumberClass;
-    switch (number) {
-      case 0:
-        bottleNumberClass = BottleNumber0;
-        break;
-      case 1:
-        bottleNumberClass = BottleNumber1;
-        break;
-      default:
-        bottleNumberClass = BottleNumber;
-        break;
+    try {
+      bottleNumberClass = eval(`BottleNumber${number}`);
+    } catch (e) {
+      bottleNumberClass = BottleNumber;
     }
 
     return new bottleNumberClass(number);
@@ -114,6 +91,16 @@ class BottleNumber1 extends BottleNumber {
 
   pronoun() {
     return "it";
+  }
+}
+
+class BottleNumber6 extends BottleNumber {
+  quantity() {
+    return "1";
+  }
+
+  container() {
+    return "six-pack";
   }
 }
 
@@ -481,9 +468,9 @@ Take one down and pass it around, 8 bottles of beer on the wall.
 Take one down and pass it around, 7 bottles of beer on the wall.
 
 7 bottles of beer on the wall, 7 bottles of beer.
-Take one down and pass it around, 6 bottles of beer on the wall.
+Take one down and pass it around, 1 six-pack of beer on the wall.
 
-6 bottles of beer on the wall, 6 bottles of beer.
+1 six-pack of beer on the wall, 1 six-pack of beer.
 Take one down and pass it around, 5 bottles of beer on the wall.
 
 5 bottles of beer on the wall, 5 bottles of beer.
